aba76b6c8176acf5c16e4337d7a6104cf32d1aa1
Arindam Nandi <arinandi2001@gmail.com>
2024-01-30 01:59:32 +0530

    added salt and hash to the password using bcryptjs and sending jwt as res

--- inotebook/backend/routes/auth.js
+++ inotebook/backend/routes/auth.js
@@ -2,6 +2,11 @@ const express = require("express");
 const router = express.Router();
 const User = require('../models/User');
 const { body, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
+
+const jwt = require('jsonwebtoken');
+const JWT_SECRET = "ArindamHere";
+
 //create a user using : POST "/api/auth/createuser". No login required
 router.post('/createuser', [
     body("name", "Enter a Valid Name").isLength({ min: 4 }),
@@ -21,14 +26,25 @@ router.post('/createuser', [
             return res.status(400).json({ error: "Sorry a user with same email exists already" })
         }
 
+        //adding a salt and hash fxn to the password and then store it in the DB
+        const salt = await bcrypt.genSalt(10);
+        secPass = await bcrypt.hash(req.body.password, salt);
+
         //create a new user
         user = await User.create({
             name: req.body.name,
-            password: req.body.password,
+            password: secPass,
             email: req.body.email,
-        })
+        });
 
-        res.json(user);
+        const data = {
+            user: {
+                id: user.id
+            }
+        }
+        const authtoken = jwt.sign(data, JWT_SECRET);
+
+        res.json(authtoken);
     }
     catch (err) {
         console.log(err.message);
