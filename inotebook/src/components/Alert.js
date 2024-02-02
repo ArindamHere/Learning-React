036cd47661661f9f536a453644bd74c79e37bf6b
Arindam Nandi <arinandi2001@gmail.com>
2024-02-02 22:44:39 +0530

    Added icon from fontawesome

---
+++ inotebook/src/components/Alert.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Alert(props) {
+    return (
+        <div>
+            <div class="alert alert-warning" role="alert">
+                {props.message}
+            </div>
+
+        </div>
+    )
+}
+
+export default Alert
+
--- inotebook/src/components/NoteItem.js
+++ inotebook/src/components/NoteItem.js
@@ -7,8 +7,16 @@ function NoteItem(props) {
 
             <div className="card my-3">
                 <div className="card-body">
-                    <h5 className="card-title">{note.title}</h5>
+                    <div className="d-flex align-items-center justify-content-between my-2">
+                        <h5 className="card-title">{note.title}</h5>
+                        <div>
+                            <i className="fa-solid fa-trash-can mx-2"></i>
+                            <i className="fa-solid fa-user-pen mx-2"></i>
+                        </div>
+                    </div>
+
                     <p className="card-text">{note.description}</p>
+
                 </div>
             </div>
         </div>
